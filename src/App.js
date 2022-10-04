1108641c9108072cb48f12416cf045839ff4ba40
zubair2001 <zubair_mansuri2001@outlook.com>
2022-10-04 23:17:28 +0500

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import styles from "./styles"
+import {Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer} from './components'
 
 function App() {
   return (
-    <>
-      App
-    </>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div> 
+
+    </div>
   );
 }
 

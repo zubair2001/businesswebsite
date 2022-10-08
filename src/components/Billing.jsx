c4365963fbe023b9f12485be0d566345d8e23755
zubair2001 <zubair_mansuri2001@outlook.com>
2022-10-08 16:20:02 +0500

    day 6

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {apple,bill,google} from '../assets'
+import styles,{layout} from '../styles'
 
 const Billing = () => {
     return (
-        <div>Billing</div>
+        <section id="product" className={layout.sectionReverse}>
+            <div className={layout.sectionImgReverse}>
+                <img src={bill} alt="bill" className='w-[100%] h-[100%] relative z-[5]' />
+
+                <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+                
+                <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
+            </div>
+
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden' /> billing & invoicing.</h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+                    <img src={apple}  alt="googlePlay" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+                    <img src={google}  alt="googlePlay" className='w-[128px] h-[42px] object-contain cursor-pointer' />
+                </div>
+            </div>
+        </section>
     )
 }
 
